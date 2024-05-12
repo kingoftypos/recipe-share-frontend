797ce7387143ca48011115a984d1385130628c81
Shreyansh mishra <mshreyansh121@gmail.com>
2024-05-12 13:40:19 +0530

    Added Hero Section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 function App() {
   return (
     <div className="">
       <Header />
+      <Home/>
       <Routes>
         <Route path="/signup" element={<SignUpPage />} />;
         <Route path="/login" element={<LoginPage />} />;
