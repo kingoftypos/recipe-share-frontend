94ef23e9468bc7bf8e253da6c9ef30d06e60fdb1
Shreyansh mishra <mshreyansh121@gmail.com>
2024-05-13 19:18:15 +0530

    added the hero section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import RecipeDetailPage from "./pages/RecipeDetailPage";
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Homepagepic from "../assets/pexels-ella-olsson-572949-1640777.jpg";
+import { Link } from "react-router-dom";
+import HeroSection from "../components/Herosection";
+const Home=()=> {
+  return (
+    <div>
+    <HeroSection/>
+    </div>
+  )
+    };
+    export default Home;
