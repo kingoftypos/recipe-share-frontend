fd85e2439e4d52fe2f232e21290fbce1e86ffea6
Shreyansh mishra <mshreyansh121@gmail.com>
2024-05-31 08:23:46 +0530

    added search bar

---
+++ src/pages/RecipesPage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Search from '../components/Search';
+
+const RecipesPage = () => {
+  return (
+    <div><Search/>
+    <div>
+        {}
+    </div>
+    </div>
+  )
+}
+
+export default RecipesPage;
